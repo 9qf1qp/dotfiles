dff8f7884b83ddc1080bb159b3e8748dda0a4c25
vidy007 <apoteraev65@gmail.com>
2024-04-07 16:53:47 +0700

    improved install script

--- install.sh
+++ install.sh
@@ -2,8 +2,27 @@
 
 if cp -rT $HOME/dotfiles $HOME/.config; then
   echo "Copying..."
+else
+  echo "Error! Aborting..."
+  exit
 fi
+
 if find $HOME/.config -mtime -1; then
   rm -rf $HOME/.config/.git $HOME/.config/README.md $HOME/.config/install.sh
   echo "Copied successful!"
+fi
+
+if git clone https://github.com/adi1090x/rofi.git; then
+  echo "Now let's install rofi scripts"
+fi
+cd $HOME/rofi/ 
+chmod +x setup.sh
+./setup.sh
+
+echo "Installing needed programs..."
+
+if sudo pacman -Sy nemo neofetch fish kitty mako neovim waybar i3 polybar waybar; then
+  cd $HOME
+  git clone https://github.com/pijulius/picom.git
+  echo "Installed successful!"
 fi
